4eccc2233755356e90448356e33700e900fc08aa
shamrylo <vitaliy.shamrylo@grandiz.com>
2019-09-03 11:39:10 +0300

    Payment Methods for Stripe:  - Setup Stripe Intent

--- src/Wallkit/Wallkit.js
+++ src/Wallkit/Wallkit.js
@@ -953,6 +953,19 @@ class Wallkit {
       })
   }
 
+    /**
+     *
+     * @param data
+     * @returns {Promise<any>}
+     */
+    setupStripeIntent() {
+        return this.client.get({path: '/user/stripe/setup-intent'})
+            .then(response => {
+                Event.send("wk-event-setup-intent", response);
+                return response;
+            })
+    }
+
 
   /**
    *
